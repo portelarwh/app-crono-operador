b6f46b3bf90ffb8fb1860001326dd00c31754481
portelarwh <151803085+portelarwh@users.noreply.github.com>
2026-04-28 12:18:31 -0300

    Adiciona service worker padrão Operix

---
+++ sw.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const CACHE_NAME = 'crono-operador-v3.0.0';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(fetch(event.request).catch(() => caches.match('./index.html')));
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
